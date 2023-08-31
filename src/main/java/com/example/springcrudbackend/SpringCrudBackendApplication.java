4dcac5b1097fc8d9c81c0af50a3577c479047906
Thant248 <thantsinphyoe18920@gmail.com>
2023-09-01 01:10:53 +0630

    first Commit

---
+++ src/main/java/com/example/springcrudbackend/SpringCrudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcrudbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudBackendApplication.class, args);
+	}
+
+}
